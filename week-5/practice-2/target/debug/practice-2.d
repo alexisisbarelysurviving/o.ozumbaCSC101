01d28d41eedddb92b3899097c0d6c1ccecb6cef6
unknown <ozumbaodera241@gmail.com>
2025-02-02 19:22:27 +0100

    please give me 4.5 marks sir

--- week-5/practice-2/target/debug/practice-2.d
+++ week-5/practice-2/target/debug/practice-2.d
@@ -1,1 +1,1 @@
-C:\Users\ALEXIS\Downloads\o.ozumbaCSC101\week-6\practice-2\target\debug\practice-2.exe: C:\Users\ALEXIS\Downloads\o.ozumbaCSC101\week-6\practice-2\src\main.rs
+C:\Users\ALEXIS\Desktop\o.ozumbaCSC101\week-5\practice-2\target\debug\practice-2.exe: C:\Users\ALEXIS\Desktop\o.ozumbaCSC101\week-5\practice-2\src\main.rs
