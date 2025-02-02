99eb56f7bdc812e287c0fc44e304b82d29b038c3
unknown <ozumbaodera241@gmail.com>
2025-02-02 15:57:44 +0100

    please give me 4.5 marks sir

--- week-8/project_2/src/main.rs
+++ week-8/project_2/src/main.rs
@@ -1,36 +1,26 @@
-
 use std::io::Write;
 
-fn main() {
-    // Data
-    let name = vec!["Oluchi Mordi", "Adams Aliyu", "Shania Bolade", "Adekunle Gold", "Blanca Edmoh"];
-    let matric_number = vec!["ACC10211111", "ECO1011101", "CSC10328828", "EEE11020202", "MEE10202001"];
-    let department = vec!["Accounting", "Economics", "Computer Science", "Electrical Engineering", "Mechanical Engineering"];
+fn main () {
+    let name = vec!["Oluchi Mordi ", "Adams Aliyu  ", "Shania Bolade", "Adekunle Gold", "Blanca Edmoh "];
+    let matric_number = vec!["ACC10211111", "ECO10110101", "CSC10328828", "EEE11020202", "MEE10202001"];
+    let department = vec!["Accounting            ", "Economics             ", "Computer science      ", "Electrical engineering", "Mechanical engineering"];
     let level = vec![300, 100, 200, 200, 100];
 
-    // Print header
-    println!("\nStudent Information:");
-    println!("{:<20} {:<15} {:<25} {:<5}", "Name", "Matric Number", "Department", "Level");
-
-    // Print student details
-    for i in 0..name.len() {
-        println!("{:<20} {:<15} {:<25} {:<5}", name[i], matric_number[i], department[i], level[i]);
+    let mut file = std::fs::File::create("pausims.txt").expect("create failed");
+    file.write_all("\t\t\tPAU SMIS\n".as_bytes()).expect("write failed");
+    
+    print!("\nWriting to file...\n");
+    let header = "     NAME    |  MAT NO.  |      DEPARTMENT      | LEVEL\n";
+    file.write_all(header.as_bytes()).expect("write failed");
+    let headerline = "________________________________________________________\n";
+    file.write_all(headerline.as_bytes()).expect("write failed");
+    
+    for i in 0..level.len()
+    {
+        let data = name[i].to_owned() + "|" + matric_number[i] + "|" + department[i] + "|" + &level[i].to_string() + "\n";
+        file.write_all(data.as_bytes()).expect("write failed");
+    
     }
-
-    // Write to file
-    let mut file = std::fs::File::create("data.txt").expect("File creation failed");
-
-    // File header
-    let mut content = String::from("PAU SMIS\n");
-    content.push_str("Name, Matric Number, Department, Level\n");
-
-    // Add student data
-    for i in 0..name.len() {
-        content.push_str(&format!("{}, {}, {}, {}\n", name[i], matric_number[i], department[i], level[i]));
-    }
-
-    // Write all data at once
-    file.write_all(content.as_bytes()).expect("Write failed");
-
+    
     println!("\nData written to file.");
 }
