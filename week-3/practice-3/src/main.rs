9654a87e26c1d13eb906eb2c4ff8a44866728342
unknown <ozumbaodera241@gmail.com>
2025-04-13 12:46:22 +0100

    practices

--- week-3/practice-3/src/main.rs
+++ week-3/practice-3/src/main.rs
@@ -3,6 +3,7 @@ fn main() {
     let interest: f32 = 8.35;
     let cost: f64 = 15000.600;
 
+
     println!("Result value is {}", result );
     println!("interest value is {}", interest);
     println!("cost is {}", cost);
